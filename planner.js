1e115bcd182d3721a159c33e8dccd6b3652de576
errorstrata <jonskevins0@gmail.com>
2026-02-07 18:04:16 -0800

    12

--- planner.js
+++ planner.js
@@ -1,16 +1,16 @@
 const send = document.getElementById('commit')
-
 send.addEventListener('click', sendData)
+
 function sendData() {
-    const task = document.getElementById('task').value
-    const date = document.getElementById('date').value
+    let task = document.getElementById('task').value
+    let date = document.getElementById('time').value
 
     let data = {
       'task': task,
       'date': date
     }
 
-    fetch('http://127.0.0.1:5000/task', {
+    fetch('http://127.0.0.1:5000/tasks', {
       method:'POST',
       headers:{
         'Content-Type':'application/json'
@@ -20,6 +20,34 @@ function sendData() {
     .then(res => res.json())
     .then(data => {
       console.log(data)
+      getData()
     })
     .catch(err => console.log(err))
 }
+
+let input = document.getElementById('data')
+function getData(){
+    fetch('http://127.0.0.1:5000/tasks',{
+      method:'GET',
+      headers:{
+        'Content-Type':'application/json'
+      }
+   })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+
+      htmldata = ''
+      data.forEach(element => {
+        htmldata += `<div class="card">
+                        <h5 class="card-title">${element.task}</h5>
+                        <p class="card-text">${element.date}</p>
+                      </div>
+                    `
+      })
+      input.innerHTML = htmldata
+    })
+    .catch(err => console.log(err))
+}
+
+window.onload = getData()
